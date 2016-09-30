db3547221aa2de14d50101842ad9888212af000f
DavidEmetres <david.martinez.martin.1995@gmail.com>
2016-09-30 14:35:26 +0200

    Arduino Input

--- Assets/Code/ArduinoInput.cs
+++ Assets/Code/ArduinoInput.cs
@@ -1,17 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
-//using System.IO.Ports;
+using System.IO.Ports;
+using System;
+using System.Threading;
 
-public class ArduinoInput : MonoBehaviour {
+public class ArduinoInput : MonoBehaviour{
 
-	/*SerialPort stream = new SerialPort("COM5", 9600);
+	SerialPort stream;
+	Thread readThread;
+	string value;
+	bool threadRunning = true;
+
+	public static bool button1;
+	public static bool button2;
+	public static bool button3;
+	public static bool button4;
 
 	void Start () {
-		stream.Open ();	
+		string[] ports = SerialPort.GetPortNames ();
+
+		stream = new SerialPort(ports[0], 9600);
+
+		stream.Open ();
+		readThread = new Thread (new ThreadStart(ReadInput));
+		readThread.Start ();
 	}
-	
-	void Update () {
-		string value = stream.ReadLine ();
-		Debug.Log (value);
-	}*/
+
+	void OnApplicationQuit() {
+		threadRunning = false;
+	}
+
+	private void ReadInput() {
+		while (threadRunning) {
+			if (stream.IsOpen) {
+				string a = stream.ReadLine();
+				string[] b = a.Split (',');
+
+				if (b [0] == "TRUE")
+					button1 = true;
+				else
+					button1 = false;
+
+				if (b [1] == "TRUE")
+					button2 = true;
+				else
+					button2 = false;
+
+				if (b [2] == "TRUE")
+					button3 = true;
+				else
+					button3 = false;
+
+				if (b [3] == "TRUE")
+					button4 = true;
+				else
+					button4 = false;
+			}
+		}
+	}
 }
