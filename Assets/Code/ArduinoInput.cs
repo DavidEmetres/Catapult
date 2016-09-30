f258414858ee08fcdbf2fc3f243e78f7a36adc53
DavidEmetres <david.martinez.martin.1995@gmail.com>
2016-09-30 10:14:08 +0200

    Config

---
+++ Assets/Code/ArduinoInput.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+//using System.IO.Ports;
+
+public class ArduinoInput : MonoBehaviour {
+
+	/*SerialPort stream = new SerialPort("COM5", 9600);
+
+	void Start () {
+		stream.Open ();	
+	}
+	
+	void Update () {
+		string value = stream.ReadLine ();
+		Debug.Log (value);
+	}*/
+}
