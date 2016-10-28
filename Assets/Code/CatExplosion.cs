a2d397d16868994e72738423ff28733075d2814f
DavidEmetres <david.martinez.martin.1995@gmail.com>
2016-10-28 10:20:02 +0200

    Added some animations

---
+++ Assets/Code/CatExplosion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatExplosion : MonoBehaviour {
+
+	float totalTime;
+	SpriteRenderer sprite;
+
+	public float duration;
+
+	void Start() {
+		totalTime = Time.time + duration;
+		sprite = GetComponent<SpriteRenderer> ();
+	}
+
+	void Update () {
+		if (Time.time >= totalTime)
+			Destroy (this.gameObject);
+
+		sprite.color = new Vector4 (sprite.color.r, sprite.color.g, sprite.color.b, sprite.color.a - 0.005f);
+	}
+}
